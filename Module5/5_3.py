aaa347384e2b85f10c21687ef491321d54c97de0
ruzil426 <ruzil426@mail.ru>
2024-06-26 23:19:42 +0300

    1

--- Module5/5_3.py
+++ Module5/5_3.py
@@ -6,6 +6,6 @@ class Building:
     def __eq__(self, other):
         return self.numberOfFloors == other.numberOfFloors or self.buildingType == other.buildingType
 
-number1 = (1, 'Частный дом')
-number2 = (10, 'МКД')
+number1 = Building(1, 'Частный дом')
+number2 = Building(10, 'МКД')
 print(number1 != number2)
