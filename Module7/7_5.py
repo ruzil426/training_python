e4128145a3176bbc13751c84877ca0e0b89f5d10
ruzil426 <ruzil426@mail.ru>
2024-08-09 21:26:48 +0300

    +

--- Module7/7_5.py
+++ Module7/7_5.py
@@ -8,6 +8,6 @@ for address, dirs, files in os.walk(directory):
         filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
         file_size = os.path.getsize(file)
-        parent_dir = os.path.dirname(file)
+        parent_dir = os.path.dirname(file_path)
         print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {file_size}, '
               f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
