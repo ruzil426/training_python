3ace78fd5396cdc3addec3321a071fd36860badb
ruzil426 <ruzil426@mail.ru>
2024-08-21 22:48:31 +0300

    2 вариант

--- Module9/9_3.py
+++ Module9/9_3.py
@@ -3,7 +3,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 # second_result =(len(x) == len(y) for x in first for y in second if first.index(x) == second.index(y))
-second_result = [len(first[i]) == len(second[i]) for i in range(len(first))]
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
 print(list(second_result))
