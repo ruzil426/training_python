826a2bb3b5309856723e6957de67fc6e288a2dab
ruzil426 <ruzil426@mail.ru>
2024-09-15 20:45:24 +0300

    1 вариант

---
+++ Module10/10_4.py
@@ -0,0 +1,53 @@
+from threading import Thread
+from random import randint
+from time import sleep
+from queue import Queue
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+class Guest(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+    def run(self):
+        sleep(randint(3,10))
+
+class Cafe:
+    def __init__(self, *tables):
+        self.tables = tables
+        self.queue = Queue()
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            for table in self.tables:
+                if not table.guest:
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    table.guest = guest
+                    break
+            else:
+                print(f"{guest.name} в очереди")
+                self.queue.put(guest)
+
+    def discuss_guests(self):
+        while self.queue.empty() or any([table.guest for table in self.tables]):
+            for table in self.tables:
+                if table.guest and not table.guest.is_alive():
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)" )
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+                if not self.queue.empty() and table.guest is None:
+                    table.guest = self.queue.get()
+                    print(f"{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+                    table.guest.start()
+
+
+tables = [Table(number) for number in range(1, 6)]
+guests_names = ['Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra']
+guests = [Guest(name) for name in guests_names]
+cafe = Cafe(*tables)
+cafe.guest_arrival(*guests)
+cafe.discuss_guests()
