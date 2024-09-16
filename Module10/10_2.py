658012d906454e7c33938242836804bcebf7f3dc
ruzil426 <ruzil426@mail.ru>
2024-09-16 23:14:44 +0300

    2 вариант

--- Module10/10_2.py
+++ Module10/10_2.py
@@ -12,13 +12,20 @@ class Knight(Thread):
         print(f'{self.name}, на нас напали!')
         while enemy != 0:
             enemy = enemy - self.power
-            sleep(1)
-            day += 1
-            print(f'{self.name} сражается {day} день (дня), осталось {enemy} воинов')
-            if enemy ==0:
-                print(f'{self.name} одержал победу спустя {day} дней(дня)!')
+            if enemy < 0:
+                print(f'{self.name} сражается {day+1} день (дня), осталось 0 воинов')
+                print(f'{self.name} одержал победу спустя {day+1} дней(дня)!')
+                break
+            else:
+                sleep(1)
+                day += 1
+                print(f'{self.name} сражается {day} день (дня), осталось {enemy} воинов')
+                if enemy == 0:
+                    print(f'{self.name} одержал победу спустя {day} дней(дня)!')
+            # elif enemy < 0:
+            #     break
 
-first_knight = Knight('Sir Lancelot', 10)
+first_knight = Knight('Sir Lancelot', 11)
 second_knight = Knight("Sir Galahad", 20)
 
 first_knight.start()
