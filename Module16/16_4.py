d60f76fcfc6e21082994c77685cbf9c64b5c9b38
ruzil426 <ruzil426@mail.ru>
2024-11-05 21:11:02 +0300

    вариант 2

--- Module16/16_4.py
+++ Module16/16_4.py
@@ -1,35 +1,44 @@
 from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
 from pydantic import BaseModel
-from typing import List
 
 app = FastAPI()
 users = []
+
 class User(BaseModel):
-    user_id: int
+    id: int
     username: str
     age: int
+
 @app.get("/users")
-async def get_all_users() -> List[User]:
+async def get_all_users() ->List[User]:
     return users
+
 @app.post("/user/{username}/{age}")
-async def create_user(username: User, age: User):
-    if not users:
-        user_id = 1
-    else:
-        user_id = str(int(max(users, key=int)) + 1)
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'User {user_id} is registered'
-@app.put("/user/{user_id}/{user_name}/{age}")
-async def update_user(user_id: User, username: User, age: User):
-    try:
-        users[user_id] = f'Имя: {username}, возраст: {age}'
-        return f'User {user_id} has been updated'
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User_id not found")
+async def create_user(username:
+                Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+                age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='35')]):
+    user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
+    user = User(id = user_id, username = username, age = age)
+    users.append(user)
+    return f'User {user_id} is registered', user
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')],
+        username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+        age: Annotated[int, Path(le=120, ge=18, description='Enter age', example='35')]):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return f'User {user_id} has been updated', user
+    raise HTTPException(status_code=404, detail='User not found')
+
 @app.delete("/user/{user_id}")
-async def delete_user(user_id):
-    try:
-        users.pop(user_id)
-        return f'User {user_id} has been deleted'
-    except IndexError:
-        raise HTTPException(status_code=404, detail="User_id not found")
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return f'User {user_id} has been deleted', user
+    raise HTTPException(status_code=404, detail="User not found")
